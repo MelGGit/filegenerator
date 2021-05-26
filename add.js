524c6e65763c7180a572ce830401738bc09b9b7b
Mel Grosser <grosser.mel@live.de>
2021-05-26 10:33:24 +0200

    stepTwo

--- add.js
+++ add.js
@@ -1,10 +1,1 @@
-const arr = process.argv
-
-const a = arr[2]
-const b = arr[3]
-
-console.log(add(a, b))
-
-function add(a, b) {
-  return Number(a) + Number(b)
-}
+function add() {}
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const functionName = process.argv[2]
+const fs = require('fs')
 
+fs.writeFileSync(`./${functionName}.js`, `function ${functionName}(){}`)
 console.log(`function ${functionName}() {}
 `)
